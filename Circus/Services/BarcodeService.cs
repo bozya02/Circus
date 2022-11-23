57eb9a667b2fbc56bb8209d1059f88ba7c1de52d
bozya02 <bozinandrey1@yandex.ru>
2022-11-23 19:34:40 +0300

    edit qrcode generator

--- Circus/Services/BarcodeService.cs
+++ Circus/Services/BarcodeService.cs
@@ -6,7 +6,7 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using Circus.DB;
-using IronBarCode;
+using QRCoder;
 
 namespace Circus.Services
 {
@@ -22,10 +22,15 @@ namespace Circus.Services
                           $"{ticket.User.FullName} " +
                           $"{ticket.Perfomance.TicketPrice} руб.";
 
-            var qrCode = BarcodeWriter.CreateBarcode(content, BarcodeWriterEncoding.QRCode);
-            qrCode.TextEncoding = Encoding.UTF8;
-            qrCode.ChangeBarCodeColor(Color.OrangeRed);
-            return qrCode.ToBitmap();
+            Bitmap qrCodeImage = null; 
+            using (QRCodeGenerator qrGenerator = new QRCodeGenerator())
+            using (QRCodeData qrCodeData = qrGenerator.CreateQrCode(content, QRCodeGenerator.ECCLevel.Q))
+            using (QRCode qrCode = new QRCode(qrCodeData))
+            {
+                qrCodeImage = qrCode.GetGraphic(25, Color.OrangeRed, Color.White, true);
+            }
+
+            return qrCodeImage;
         }
     }
 }
